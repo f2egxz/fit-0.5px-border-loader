58a6423aed390bafc45ba7933dbb9ae2b28b70e5
郭栩志 <568846698@qq.com>
2018-05-29 16:01:08 +0800

    save

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 module.exports = function(content, map, meta) {
-    content = content.replace(/(border.*?):\s+0\.5px(.*?);/g, function(
-        string,
-        $1,
-        $2
-    ) {
-        const STRING = `${$1}: 1px${$2};
+  content = content.replace(/(border.*?):\s+0\.5px(.*?);/g, function(
+    string,
+    $1,
+    $2
+  ) {
+    const STRING = `${$1}: 1px${$2};
         @media (-webkit-min-device-pixel-ratio: 2), (min-resolution: 2dppx){
                 ${string}
         }
-        `
-        return STRING
-    })
-    this.callback(null, content, map, meta)
-}
+        `;
+    return STRING;
+  });
+  this.callback(null, content, map, meta);
+};
